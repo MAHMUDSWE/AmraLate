4f227be6aa17f5ea0b3799d0cecce5322650ebcf
rajuswesust <singhoraju8@gmail.com>
2023-10-13 19:39:04 +0600

    adding

---
+++ backend/src/main/java/com/amralate/practice/AmraLateApplication.java
@@ -0,0 +1,13 @@
+package com.amralate.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmraLateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmraLateApplication.class, args);
+	}
+
+}
